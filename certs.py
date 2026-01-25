5edd0a22f2fd70be32bddc1ca6830d6738494ee4
SemkaShr <shirokov29072009@gmail.com>
2026-01-25 10:47:11 +0000

    nothing changed

--- certs.py
+++ certs.py
@@ -2,3 +2,5 @@ import app.haproxy as haproxy
 
 hap = haproxy.HAProxy(None)
 print(hap.check_certificate('captcha.qwertyx.host'))
+print(hap.check_certificate('systems.qwertyx.host'))
+print(hap.check_certificate('uptime.qwertyx.host'))
